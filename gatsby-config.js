0db3981ef91c5d5a8a8b72ef79cbe9a54f721673
Adro.codes <hurricane.interactive@gmail.com>
2018-02-11 14:35:35 +1100

    Removing typography include
    Also removed 'fonts' in 'gatsby-source-filesystem'

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,15 +10,8 @@ module.exports = {
       options: {
         path: `${__dirname}/src/pages`,
         name: 'pages',
-        fonts: ['Raleway', 'sans-serif'],
       }
     },
     `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
-    }, 
   ],
 }
