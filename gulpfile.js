152039ac6fa3c862d112a68a5fd4ed106c0363d6
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2018-02-24 00:03:12 +1100

    changed file name of html as to not confuse the browser
    contact.html to contact-hidden-file.html

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ var gulp = require('gulp');
 gulp.task('default', function() {
   // place code for your default task here
 
-  return gulp.src('src/contact.html')
+  return gulp.src('src/contact-hidden-file.html.html')
     .pipe(gulp.dest('public/'));
 });
