cc4e21c1bfc32fa73b4b8182d453ada3a7c448c6
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2018-03-02 18:48:14 +1100

    added meta tags...oops

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -115,10 +115,14 @@ const Footer = () => (
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Ignite Driving | South Canberra Learner Driving School"
+      title="Ignite Driving | South Canberra Driving School"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Based in South Canberra, Ignite Driving aims to help Learner drivers transition to confident and safe P plate drivers using thorough and informative teaching techniques' },
+        { name: 'keywords', content: 'Driving, Keys2Drive, South Canberra, Instructor, Learner, Driving Lessons, Help, Teach, Learn' },
+        { property: 'og:title', content: 'Ignite Driving | South Canberra Driving School'},
+        { property: 'og:description', content: 'Based in South Canberra, Ignite Driving aims to help Learner drivers transition to confident and safe P plate drivers using thorough and informative teaching techniques'},
+        { property: 'og:url', content: 'ignitedriving.com.au'},
+        { property: 'og:image', content: 'http://d33wubrfki0l68.cloudfront.net/6e0d672d0e0bd74654ea9867cd34324c9213a020/66160/static/bgimg.43ab0aac.jpg'}
       ]}
       script={[
         {"src": "https://use.fontawesome.com/releases/v5.0.6/js/all.js", "type": "text/javascript"},
