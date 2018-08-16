e7d9e2e31c120df426658595a5f0860d60f0591f
Luke Secomb <luke.secomb@live.com>
2018-08-17 00:04:18 +1000

    added terms and conditions pdf

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import logo from "../assets/logo.svg"
-import bgimg from "../assets/bgimg.jpg"
 import adta from "../assets/adta.png"
 import k2d from "../assets/k2d.png"
 import { NavLink } from 'react-router-dom'
@@ -17,9 +16,7 @@ import instagram_01 from '../assets/insta/instagram_post_02.jpg'
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import { faHome, faCar, faDollarSign, faInfo, faPhone } from '@fortawesome/fontawesome-free-solid'
-import { faInstagram } from '@fortawesome/fontawesome-free-solid'
-import { faFacebookSquare } from '@fortawesome/fontawesome-free-solid'
-import { faGoogle } from '@fortawesome/fontawesome-free-solid'
+import Terms from '../assets/ignite-driving-school-terms-and-conditions.pdf'
 
 import "../assets/slick/slick.css";
 import "../assets/slick/slick-theme.css";
@@ -63,61 +60,64 @@ const Header = () => (
   </nav>
 )
 
-const Footer = () => (
-  <footer className="footer">
-    <section className="footerMain">
-      <div>
-        <div className="footerNav">
-          <ul>
-            <li>Ignite Driving</li>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/whatweoffer">What We Offer</NavLink></li>
-            <li><NavLink to="/pricing">Pricing</NavLink></li>
-            <li><NavLink to="/about">About</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
-          </ul>
-        </div>
-        <div className="footerContact">
-          <ul className="footerContactChild">
-            <li>Contact</li>
-            <li><a to="tel:+61434833131" itemprop="telephone">Phone: 0434 833 131</a></li>
-            <li><a href="&#x6d;&#x61;&#x69;&#108;&#x74;&#111;&#58;&#109;&#x61;&#x72;&#x67;&#x61;&#114;&#x65;&#116;&#x40;&#x69;&#x67;&#110;&#x69;&#116;&#101;&#100;&#x72;&#x69;&#118;&#x69;&#x6e;&#103;&#46;&#x63;&#111;&#109;&#x2e;&#x61;&#x75;">&#109;&#97;&#x72;&#x67;&#x61;&#114;&#101;&#x74;&#x40;&#x69;&#103;&#110;&#105;&#116;&#x65;&#x64;&#x72;&#105;&#x76;&#x69;&#x6e;&#x67;&#x2e;&#99;&#111;&#x6d;&#46;&#x61;&#117;</a></li>
-          </ul>
-          <ul className="footerSocials">
-            <li><a href="https://www.facebook.com/ignitedriving" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab","facebook"]}/></a></li>
-            <li><a href="https://www.instagram.com/ignitedriving/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab","instagram"]}/></a></li>
-          </ul>
-        </div>
-        <a href="https://www.adta.com.au/"  target="_blank" className="img_adta"><img src={adta} alt="ADTA Logo"/></a>
-        <a href="https://www.keys2drive.com.au/"  target="_blank" className="img_k2d"><img src={k2d} alt="K2D Logo - coloured to match the ignite driving branding"/></a>
-      </div>
-      <div className="footerInstagram">
-        <div>
-          <a href="https://www.instagram.com/ignitedriving/" target="_blank" rel="noopener noreferrer"><img src={instagram_06} alt="January all booked out but luckily the $10 off your first lesson offer has been extended through February "/></a>
-          <a href="https://www.instagram.com/ignitedriving/" target="_blank" rel="noopener noreferrer"><img src={instagram_05} alt="Merry Christmas.  A bit of fun. Ignite Driving will help explain some of these gauges properly during your lessons."/></a>
-          <a href="https://www.instagram.com/ignitedriving/" target="_blank" rel="noopener noreferrer"><img src={instagram_04} alt="Picked up the new business cards today.  Looking good thanks to @luke_secomb_digital .  He's the man for design!"/></a>
+class Footer extends Component {
+  render() {
+    let getCurrentYear = new Date().getFullYear();
+    return (
+      <footer className="footer">
+          <section className="footerMain">
+            <div>
+              <div className="footerNav">
+                <ul>
+                  <li>Ignite Driving</li>
+                  <li><NavLink to="/">Home</NavLink></li>
+                  <li><NavLink to="/whatweoffer">What We Offer</NavLink></li>
+                  <li><NavLink to="/pricing">Pricing</NavLink></li>
+                  <li><NavLink to="/about">About</NavLink></li>
+                  <li><NavLink to="/contact">Contact</NavLink></li>
+                </ul>
+              </div>
+              <div className="footerContact">
+                <ul className="footerContactChild">
+                  <li>Contact</li>
+                  <li><a to="tel:+61434833131" itemprop="telephone">Phone: 0434 833 131</a></li>
+                  <li><a href="&#x6d;&#x61;&#x69;&#108;&#x74;&#111;&#58;&#109;&#x61;&#x72;&#x67;&#x61;&#114;&#x65;&#116;&#x40;&#x69;&#x67;&#110;&#x69;&#116;&#101;&#100;&#x72;&#x69;&#118;&#x69;&#x6e;&#103;&#46;&#x63;&#111;&#109;&#x2e;&#x61;&#x75;">&#109;&#97;&#x72;&#x67;&#x61;&#114;&#101;&#x74;&#x40;&#x69;&#103;&#110;&#105;&#116;&#x65;&#x64;&#x72;&#105;&#x76;&#x69;&#x6e;&#x67;&#x2e;&#99;&#111;&#x6d;&#46;&#x61;&#117;</a></li>
+                </ul>
+                <ul className="footerSocials">
+                  <li><a href="https://www.facebook.com/ignitedriving" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab","facebook"]}/></a></li>
+                  <li><a href="https://www.instagram.com/ignitedriving/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={["fab","instagram"]}/></a></li>
+                </ul>
+              </div>
+              <a href="https://www.adta.com.au/"  target="_blank" className="img_adta"><img src={adta} alt="ADTA Logo"/></a>
+              <a href="https://www.keys2drive.com.au/"  target="_blank" className="img_k2d"><img src={k2d} alt="K2D Logo - coloured to match the ignite driving branding"/></a>
+            </div>
+            <div className="footerInstagram">
+              <div>
+                <a href="https://www.instagram.com/ignitedriving/" target="_blank" rel="noopener noreferrer"><img src={instagram_06} alt="January all booked out but luckily the $10 off your first lesson offer has been extended through February "/></a>
+                <a href="https://www.instagram.com/ignitedriving/" target="_blank" rel="noopener noreferrer"><img src={instagram_05} alt="Merry Christmas.  A bit of fun. Ignite Driving will help explain some of these gauges properly during your lessons."/></a>
+                <a href="https://www.instagram.com/ignitedriving/" target="_blank" rel="noopener noreferrer"><img src={instagram_04} alt="Picked up the new business cards today.  Looking good thanks to @luke_secomb_digital .  He's the man for design!"/></a>
 
-          <a href="https://www.instagram.com/ignitedriving/" target="_blank" rel="noopener noreferrer"><img src={instagram_03} alt="Instagram Post"/></a>
-          <a href="https://www.instagram.com/ignitedriving/" target="_blank" rel="noopener noreferrer"><img src={instagram_02} alt="Instagram Post"/></a>
-          <a href="https://www.instagram.com/ignitedriving/" target="_blank" rel="noopener noreferrer"><img src={instagram_01} alt="Instagram Post"/></a>
-        </div>
-        <a href="https://www.instagram.com/ignitedriving/">See more on Instagram</a>
-      </div>
-    </section>
-    <section className="footerBottom">
-      <div>
-        <ul>
-          <li><a href="/">Privacy</a></li>
-          <li><a href="/">T&C</a></li>
-          <li><a href="/">Sitemap</a></li>
-        </ul>
-        <a href="/">@ 2018 All Rights Reserved</a>
-        <a href="https://lukesecomb.digital/" target="_blank">Created by Luke Secomb Digital</a>
-      </div>
-    </section>
-  </footer>
-)
-
+                <a href="https://www.instagram.com/ignitedriving/" target="_blank" rel="noopener noreferrer"><img src={instagram_03} alt="Instagram Post"/></a>
+                <a href="https://www.instagram.com/ignitedriving/" target="_blank" rel="noopener noreferrer"><img src={instagram_02} alt="Instagram Post"/></a>
+                <a href="https://www.instagram.com/ignitedriving/" target="_blank" rel="noopener noreferrer"><img src={instagram_01} alt="Instagram Post"/></a>
+              </div>
+              <a href="https://www.instagram.com/ignitedriving/">See more on Instagram</a>
+            </div>
+          </section>
+          <section className="footerBottom">
+            <div>
+              <ul>
+                <li><a href={Terms} download>Terms & Conditions</a></li>
+                <li><a href="/">Sitemap</a></li>
+              </ul>
+              <a href={Terms}>{getCurrentYear} All Rights Reserved</a>
+              <a href="https://lukesecomb.digital/" target="_blank">Created by Luke Secomb Digital</a>
+            </div>
+          </section>
+        </footer>
+    )
+  }
+}
 
 const TemplateWrapper = ({ children }) => (
   <div>
