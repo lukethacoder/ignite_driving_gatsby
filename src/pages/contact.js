541130ff689f1848d23d045beb97345443f22a26
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2019-03-04 08:39:54 +1100

    data typo

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ import brands from '@fortawesome/fontawesome-free-solid';
 
 const encode = data => {
   return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(dat[key]))
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
     .join('&');
 };
 
